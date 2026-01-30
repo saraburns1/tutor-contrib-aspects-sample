6319799c0bb2081613aa0eb7d13efa34f4e59894
Sara Burns <sburns@axim.org>
2026-01-30 13:45:07 -0500

    chore: add ruff requirement for formatting

--- tutoraspects_sample/plugin.py
+++ tutoraspects_sample/plugin.py
@@ -154,7 +154,9 @@ hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
 
 # For each file in tutoraspects_sample/patches,
 # apply a patch based on the file's name and contents.
-for path in glob(str(importlib_resources.files("tutoraspects_sample") / "patches" / "*")):
+for path in glob(
+    str(importlib_resources.files("tutoraspects_sample") / "patches" / "*")
+):
     with open(path, encoding="utf-8") as patch_file:
         hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
 
